f01756ce7c371e98ebee146e012c04bc4cbb316f
JT Longino <jlongino@gmail.com>
2020-10-14 20:19:16 -0500

    Add scholarship list

--- footer_scholarship.js
+++ footer_scholarship.js
@@ -2,7 +2,52 @@
   var scholarshipRows = 1;
 
   // Key is name of scholarship, value is true if can be used for living expenses, false otherwise
-  var scholarships = {"Sch A":true, "Sch B":false, "Sch C":false};
+  var scholarships = {
+    "Americorps Scholarship":true,
+    "Assistance League Scholarships":false,
+    "Barbara Tidwell Scholarship (Texas State)":false,
+    "Brown Scholarship (St. Edwards)":false,
+    "Concordia Grant":false,
+    "Edwardian Grant":false,
+    "Exemplary Presidents Achievement Scholarship (UT)":true,
+    "Faculty Scholarship (St. Edwards)":false,
+    "Federal Pell Grant":true,
+    "Federal Supplemental Education Opportunity Grant (SEOG)":true,
+    "Federal Supplemental Grant (Southwestern)":true,
+    "Foundation Communities":false,
+    "Goodwill":false,
+    "HACA":false,
+    "Hazelwood Scholarship (Texas Veterans Commission)":false,
+    "Hindu Charities Scholarship (ACC)":true,
+    "Howard Sides Scholarship (Southwestern)":false,
+    "Institutional grant (St. Edwards)":false,
+    "Iraq and Afghanistan Service Grant":false,
+    "Maverick (St. Edwards)":false,
+    "McDonald’s Scholarship":true,
+    "Service Scholarship (St. Edwards)":false,
+    "Simmang Scholarship SON":false,
+    "St. David’s Foundation Scholarship":true,
+    "State Top 10%":true,
+    "TEACH":true,
+    "Texas Aggie Scholarship":false,
+    "Texas Association of Chicanos in Higher Education Scholarship":false,
+    "Texas Equal Opportunity Grant":true,
+    "Texas Excellence Grant":true,
+    "Texas Grant":true,
+    "Texas Grant - Continuing":true,
+    "Texas Public Education Grant":true,
+    "Texas State Achievement Scholarship":true,
+    "Texas State Tuition Grant":true,
+    "TX Tuition Equalization Grant":true,
+    "Transfer Scholar Award":false,
+    "Tuition Scholarship for Resident Student":false,
+    "Undergrad Assistance Grant":false,
+    "U.S. Department of Veteran Affairs":true,
+    "Walther Grant":false,
+    "W.E.B. DuBois Scholar (Huston-Tillotson)":false,
+    "Workforce":false,
+    "Your Private Scholarship (St. Edwards)":false,
+  };
   var schList = document.getElementById('scholarshipList');
   var scholarshipsUpper = {};
   for (var key in scholarships) {
