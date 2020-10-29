80b565080a70044cc692674126a85ce18136b117
JT Longino <jlongino@gmail.com>
2020-10-28 19:33:50 -0500

    Expense placeholders

--- footer_expense.js
+++ footer_expense.js
@@ -2,19 +2,87 @@
   var expenseRows = 1;
 
   // Key is name of expense, [valid for tuition, valid for credit]
-  var expenses = {
-    "Expense Both": [true, true],
-    "Expense Credit": [false, true],
-    "Expense Tuition": [true, false]
+  var expensesAll = {
+    "none": {
+      "display-name": "Select School"
+    },
+    "ut-austin" : {
+      "display-name" : "UT Austin",
+      "UT Expense Both": [true, true],
+      "UT Expense Credit": [false, true],
+      "UT Expense Tuition": [true, false]
+    },
+    "acc" : {
+      "display-name" : "ACC",
+      "ACC Expense Both": [true, true],
+      "ACC Expense Credit": [false, true],
+      "ACC Expense Tuition": [true, false]
+    },
+    "texas-tech" : {
+      "display-name" : "Texas Tech",
+      "Tech Expense Both": [true, true],
+      "Tech Expense Credit": [false, true],
+      "Tech Expense Tuition": [true, false]
+    },
+    "other": {
+      "display-name": "Other"
+    }
   };
-  var expList = document.getElementById('expensesList');
+
+  var expenses = expensesAll["none"];
   var expensesUpper = {};
-  for (var key in expenses) {
-    var keyStr = key.toString();
-    var option = document.createElement('option');
-    option.value = key;
-    expList.appendChild(option);
-    expensesUpper[key.toUpperCase()] = expenses[key];
+
+  function populateExpenseSelect() {
+    var schoolSelect = document.getElementById("exp-school-select");
+    // Remove all entries from select
+    for (var i = schoolSelect.length-1; i >= 0; i--){
+      var option = schoolSelect.options[i];
+      option.remove();
+    }
+    // Add new options based on expenses keys
+    for (var key in expensesAll) {
+      var displayName = expensesAll[key]["display-name"];
+      var option = document.createElement('option');
+      option.value = key;
+      option.label = displayName;
+      option.selected = false;
+      if (key == "none") option.selected = true;
+      schoolSelect.appendChild(option);
+    }
+    updateExpenseList();
+  }
+    
+  function updateExpenseList() {
+    // Get the new expense list
+    var selectedOption = null;
+    var schoolSelect = document.getElementById("exp-school-select");
+    for (var i = 0; i < schoolSelect.length; i++) {
+      var option = schoolSelect.options[i];
+      if (option.selected) {
+        selectedOption = option;
+      }
+    }
+    expenses = expensesAll[selectedOption.value];
+    
+    // Clear expense list
+    var expList = document.getElementById('expensesList');
+    for (var i = expList.options.length-1; i >= 0; i--){
+      var option = expList.options[i];
+      option.remove();
+    }
+    // Clear expensesUpper
+    expensesUpper = {};
+    
+    // Populate expense list
+    for (var key in expenses) {
+      if (key.toString() != "display-name") {
+        var option = document.createElement('option');
+        option.value = key;
+        expList.appendChild(option);
+        expensesUpper[key.toUpperCase()] = expenses[key];
+      }
+    }
+    updateExpensesTable();
   }
   function addExpenseRow() {
     expenseRows++;
@@ -70,15 +138,19 @@
       }
     }
   }
-
   function updateExpensesTable() {
     updateExpenseRows();
     updateOutputs();
   }
+
   var elements = document.getElementsByClassName("exp-input-name");
   for (var i = 0; i < elements.length; i++) {
       elements[i].addEventListener('change', updateExpensesTable, false);
   }
   var expenseButton = document.getElementById("add-expense");
   expenseButton.addEventListener('click', addExpenseRow, false);
+  var schoolSelect = document.getElementById("exp-school-select");
+  schoolSelect.addEventListener('click', updateExpenseList, false);
+  document.addEventListener('DOMContentLoaded', populateExpenseSelect, false);
+
 </script>
